a0c345858f7085a2cdbcf76528595c26cf5f9b7e
curtis <laurencechadwell@gmail.com>
2021-01-03 21:25:40 -0600

    Added recipe routes

--- receipe/app/controllers/recipes_controller.rb
+++ receipe/app/controllers/recipes_controller.rb
@@ -4,7 +4,29 @@ class RecipesController < ApplicationController
         recipes = Recipe.all
         render json: recipes, include: :catagories
     end
+
+    def show
+        recipe = Recipe.find(id: params[:id])
+        options ={
+            include: [:catagory]
+        }
+        render json: recipe
+    end
+
+
+    def create
+        recipe = Recipe.create(rec_params)
+        if recipe.save
+            render json: recipe
+        else
+            render json: { error: "Couldn't save" }
+        end
+    end
     
+
+    def rec_params
+        params.permit(:catagory_id,:name,:ingredients,:chef_name,:origin,catagory_attribute:[:name])
+    end
     
     
     
--- receipe/config/routes.rb
+++ receipe/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 resources :catagory
-
+resources :recipes
 get '/catagories', to: 'catagories#index'
+
+get '/recipes', to: 'recipes#index'
+post '/recipes', to: 'recipes#create'
+get '/recipes/:id', to: 'recipes#show'
+
 
 end
