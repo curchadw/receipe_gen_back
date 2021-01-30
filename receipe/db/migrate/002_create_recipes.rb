d4909bdb339e516362056fa191a01a15e8c5abf3
Curtis Laurence Chadwell <43461325+curchadw@users.noreply.github.com>
2021-01-30 17:54:49 -0600

    Catching

--- receipe/db/migrate/002_create_recipes.rb
+++ receipe/db/migrate/002_create_recipes.rb
@@ -5,6 +5,7 @@ class CreateRecipes < ActiveRecord::Migration[6.0]
             t.text :ingredients
             t.string :chef_name
             t.string :origin
+            t.string :instructions
             t.integer :category_id
             t.timestamps
         end    
