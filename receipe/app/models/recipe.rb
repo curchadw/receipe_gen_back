267c07b182b7f66c6d5a9a9e8a021f35b5fc37d5
curtis <laurencechadwell@gmail.com>
2021-01-31 21:48:53 -0600

    Still fixing frontend rendering issue

--- receipe/app/models/recipe.rb
+++ receipe/app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ApplicationRecord
     belongs_to :category
     accepts_nested_attributes_for :category
+    validates :name,:ingredients,:chef_name,:origin,:instructions, presence: true
 
 end
